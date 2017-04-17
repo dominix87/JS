32009d64bbf927cf56aa6ad899e079fd6a857741
Dominix <dominix@list.ru>
2017-04-17 17:06:53 +0300

    1-3

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+var customArray = [4, 8, 7];
+
+console.log(customArray);
+customArray = 0;
+console.log(customArray);
+
+var age = 25;
+console.log(age);
+age = "nothing";
+console.log(age);
+
+var object1 = {};
+
+object1.name = 'Alex';
+object1.age = 87;
+console.log(object1.name, object1.age);
+console.log(object1.age);
