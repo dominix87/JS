c0bce165bcb734135a52e436afb9754da2e76bd1
Dominix <dominix@list.ru>
2017-04-17 19:54:59 +0300

    4-7

--- JavaScript.js
+++ JavaScript.js
@@ -15,3 +15,14 @@ object1.name = 'Alex';
 object1.age = 87;
 console.log(object1.name, object1.age);
 console.log(object1.age);
+
+var student = "Alex";
+console.log(student);
+student = "Koval Alex";
+console.log(student);
+
+var name = prompt("Введите свое имя");
+alert("Добро пожаловать на сайт " + name + "!");
+confirm("Вы и правда собираетесь загрузить данную страницу?");
+
+console.log(+'9' + 9);
