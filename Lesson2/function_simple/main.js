5588d2174e5278e5b0bc1dc8234d4d63eec9a7ff
Dominix <dominix@list.ru>
2017-04-24 09:41:48 +0300

    start advanced

--- Lesson2/function_simple/main.js
+++ Lesson2/function_simple/main.js
@@ -4,9 +4,39 @@
 //double2(7987233);
 //double2("dfs");
 //range(2, 14);//????????????????????????
-tripple(41);
+//tripple(41);
+//oddEven(9);
+//rangeOdd(3, 55);
+enter9();
 
 
+
+function enter9(){
+	var n = prompt("ввдеите число 9");
+	if (n == 9) return alert("Ну наконе-то ты ввел эту 9-ку");
+	else {
+		alert("а вот и нет!");
+		enter9();
+	}
+}
+
+function rangeOdd(x, y){ // работает нормально, если закомментить  в предидущей функции console.log
+	for(var i = x; i <= y; i++){
+		if (oddEven(i)) console.log(i);
+	}
+
+}
+
+function oddEven(x){
+	if (x % 2 == 0) {
+		console.log(true); //Для наглядности
+		return true
+	}	
+	else {
+		console.log(false); //Для наглядности
+		return false
+	} 
+}
 
 function tripple(x){
 	console.log( 3 * double2(x) );
